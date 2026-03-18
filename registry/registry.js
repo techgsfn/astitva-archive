9be2b467afc1494cb08a91fa2744fb23def77b70
GSFN Bharat <techgsfn@gmail.com>
2026-03-18 18:25:46 +0530

    Update registry.js

--- registry/registry.js
+++ registry/registry.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
 
+let out = { publications: [] };
+
+["mono","svs","vhc","svg","ast"].forEach(type => {
+  const dir = `data/${type}`;
+  if (!fs.existsSync(dir)) return;
+
+  fs.readdirSync(dir).forEach(file => {
+    const d = JSON.parse(fs.readFileSync(`${dir}/${file}`));
+
+    out.publications.push({
+      title: d.title,
+      url: `/${type}/${d.files.slug}`,
+      year: d.year,
+      author: d.author.name
+    });
+  });
+});
+
+fs.mkdirSync("registry",{recursive:true});
+fs.writeFileSync("registry/registry.json", JSON.stringify(out,null,2));
+
+console.log("✔ Registry created");
