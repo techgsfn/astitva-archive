d787a35e11008da8928d629a26fca3e28041b185
GSFN Bharat <techgsfn@gmail.com>
2026-03-18 18:27:44 +0530

    Update sitemap.js

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
 
+let urls = [];
+
+["mono","svs","vhc","svg","ast"].forEach(type => {
+  const dir = `data/${type}`;
+  if (!fs.existsSync(dir)) return;
+
+  fs.readdirSync(dir).forEach(file => {
+    const d = JSON.parse(fs.readFileSync(`${dir}/${file}`));
+    urls.push(`https://astitva.gsfn.in/${type}/${d.files.slug}`);
+  });
+});
+
+const xml = `<?xml version="1.0"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(u=>`<url><loc>${u}</loc></url>`).join("")}
+</urlset>`;
+
+fs.writeFileSync("sitemap.xml", xml);
+
+console.log("✔ Sitemap created");
